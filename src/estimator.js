c0655777a01d5ab6306aec3411c6e05c4f3b0d1c
Kayode Gabriel Abiodun <kayodegabriela@gmail.com>
2020-04-08 21:14:18 +0100

    Fourth Attempt

--- src/estimator.js
+++ src/estimator.js
@@ -11,18 +11,20 @@ const input = {
   population: 66622705,
   totalHospitalBeds: 1380614
 };
+
+
 const infectionsByRequestedTime = (data, theCurrentlyInfected) => {
   let infected = null;
   const { periodType, timeToElapse } = data;
   switch (periodType) {
     case 'weeks':
-      infected = theCurrentlyInfected * 2 ** Math.floor((timeToElapse * 7) / 3);
+      infected = theCurrentlyInfected * (2 ** (Math.floor((timeToElapse * 7) / 3)));
       break;
     case 'months':
-      infected = theCurrentlyInfected * 2 ** Math.floor((timeToElapse * 30) / 3);
+      infected = theCurrentlyInfected * (2 ** (Math.floor((timeToElapse * 30) / 3)));
       break;
     default:
-      infected = theCurrentlyInfected * 2 ** Math.floor(timeToElapse / 3);
+      infected = theCurrentlyInfected * (2 ** (Math.floor(timeToElapse / 3)));
   }
   return infected;
 };
