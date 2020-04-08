32bd40bb97ad702b2fab8d72b067b7d97149e0e1
Kayode Gabriel Abiodun <kayodegabriela@gmail.com>
2020-04-08 14:49:35 +0100

    First Attempt

--- src/estimator.js
+++ src/estimator.js
@@ -17,17 +17,14 @@ const infectionsByRequestedTime = (data, theCurrentlyInfected) => {
   let infected = null;
   const { periodType, timeToElapse } = data;
   switch (periodType) {
-    case 'days':
-      infected = theCurrentlyInfected * (2 ** Math.floor(timeToElapse / 3));
-      break;
     case 'weeks':
-      infected = theCurrentlyInfected * (2 ** Math.floor((timeToElapse * 7) / 3));
+      infected = theCurrentlyInfected * (2 ** Math.round((timeToElapse * 7) / 3));
       break;
     case 'months':
-      infected = theCurrentlyInfected * (2 ** Math.floor((timeToElapse * 30) / 3));
+      infected = theCurrentlyInfected * (2 ** Math.round((timeToElapse * 30) / 3));
       break;
     default:
-      infected = theCurrentlyInfected * (2 ** Math.floor(timeToElapse / 3));
+      infected = theCurrentlyInfected * (2 ** Math.round(timeToElapse / 3));
   }
   return infected;
 };
