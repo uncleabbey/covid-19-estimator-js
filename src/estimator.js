5d9568f68c37f579f2fb9475ca99c924ccfc07d0
Kayode Gabriel Abiodun <kayodegabriela@gmail.com>
2020-04-08 21:42:12 +0100

    Fifth Attempt

--- src/estimator.js
+++ src/estimator.js
@@ -1,130 +1,65 @@
-const input = {
-  region: {
-    name: 'Africa',
-    avgAge: 19.7,
-    avgDailyIncomeInUSD: 5,
-    avgDailyIncomePopulation: 0.71
-  },
-  periodType: 'days',
-  timeToElapse: 58,
-  reportedCases: 674,
-  population: 66622705,
-  totalHospitalBeds: 1380614
-};
+const covid19ImpactEstimator = (data) => {
+  const impact = {};
+  const severeImpact = {};
 
-
-const infectionsByRequestedTime = (data, theCurrentlyInfected) => {
-  let infected = null;
-  const { periodType, timeToElapse } = data;
-  switch (periodType) {
-    case 'weeks':
-      infected = theCurrentlyInfected * (2 ** (Math.round((timeToElapse * 7) / 3)));
-      break;
-    case 'months':
-      infected = theCurrentlyInfected * (2 ** (Math.round((timeToElapse * 30) / 3)));
-      break;
-    default:
-      infected = theCurrentlyInfected * (2 ** (Math.round(timeToElapse / 3)));
+  let numberOfDays;
+  if (data.periodType === 'days') {
+    numberOfDays = data.timeToElapse;
+  } else if (data.periodType === 'weeks') {
+    numberOfDays = 7 * data.timeToElapse;
   }
-  return infected;
-};
-
-const currentlyInfected = (reportedCases, estimate) => reportedCases * estimate;
-
-const severeCasesByRequestedTime = (time) => (time * 0.15);
-
-const hospitalBedsByRequestedTime = (data, severity) => {
-  const { totalHospitalBeds } = data;
-  const availableBeds = totalHospitalBeds * 0.35;
-  return availableBeds - severity;
-};
-
-const casesForICUByRequestedTime = (infections) => infections * 0.05;
-
-const casesForVentilatorsByRequestedTime = (infections) => infections * 0.02;
-
-const dollarsInFlight = (data, infection) => {
-  const { avgDailyIncomePopulation, avgDailyIncomeInUSD } = data.region;
-  const { periodType, timeToElapse } = data;
-  let dollars;
-  switch (periodType) {
-    case 'weeks':
-      dollars = infection
-        * avgDailyIncomePopulation
-        * avgDailyIncomeInUSD
-        * (timeToElapse * 7);
-      break;
-    case 'months':
-      dollars = infection
-        * avgDailyIncomePopulation
-        * avgDailyIncomeInUSD
-        * (timeToElapse * 30);
-      break;
-    default:
-      dollars = infection
-        * avgDailyIncomePopulation
-        * avgDailyIncomeInUSD
-        * timeToElapse;
+  if (data.periodType === 'months') {
+    numberOfDays = 30 * data.timeToElapse;
   }
-  return dollars;
-};
 
-const covid19ImpactEstimator = (data = input) => {
-  const { reportedCases } = data;
-  return {
+  impact.currentlyInfected = data.reportedCases * 10;
+  severeImpact.currentlyInfected = data.reportedCases * 50;
+
+  const exponent = Math.floor(numberOfDays / 3);
+
+  impact.infectionsByRequestedTime = Math.floor(
+    impact.currentlyInfected * 2 ** exponent
+  );
+
+  severeImpact.infectionsByRequestedTime = Math.floor(
+    severeImpact.currentlyInfected * 2 ** exponent
+  );
+
+  impact.severeCasesByRequestedTime = 0.15 * impact.infectionsByRequestedTime;
+
+  severeImpact.severeCasesByRequestedTime = 0.15 * severeImpact.infectionsByRequestedTime;
+
+  const bedAvailability = 0.35 * data.totalHospitalBeds;
+
+  impact.hospitalBedsByRequestedTime = bedAvailability - impact.severeCasesByRequestedTime;
+
+  severeImpact.hospitalBedsByRequestedTime = bedAvailability
+    - severeImpact.severeCasesByRequestedTime;
+
+  impact.casesForICUByRequestedTime = 0.05 * impact.infectionsByRequestedTime;
+
+  severeImpact.casesForICUByRequestedTime = 0.05 * severeImpact.infectionsByRequestedTime;
+
+  impact.casesForVentilatorsByRequestedTime = 0.02 * impact.infectionsByRequestedTime;
+
+  severeImpact.casesForVentilatorsByRequestedTime = 0.02 * severeImpact.infectionsByRequestedTime;
+
+  const majorityEarning = data.region.avgDailyIncomePopulation;
+  const avgDailyIncome = data.region.avgDailyIncomeInUSD;
+
+  impact.dollarsInFlight = impact.infectionsByRequestedTime
+    * majorityEarning * avgDailyIncome * numberOfDays;
+
+  severeImpact.dollarsInFlight = severeImpact.infectionsByRequestedTime
+    * majorityEarning * avgDailyIncome * numberOfDays;
+
+  const result = {
     data,
-    impact: {
-      currentlyInfected: currentlyInfected(reportedCases, 10),
-      infectionsByRequestedTime: infectionsByRequestedTime(
-        data,
-        currentlyInfected(reportedCases, 10)
-      ),
-      severeCasesByRequestedTime: severeCasesByRequestedTime(
-        infectionsByRequestedTime(data, currentlyInfected(reportedCases, 10))
-      ),
-      hospitalBedsByRequestedTime: hospitalBedsByRequestedTime(
-        data,
-        severeCasesByRequestedTime(
-          infectionsByRequestedTime(data, currentlyInfected(reportedCases, 10))
-        )
-      ),
-      casesForICUByRequestedTime: casesForICUByRequestedTime(
-        infectionsByRequestedTime(data, currentlyInfected(reportedCases, 10))
-      ),
-      casesForVentilatorsByRequestedTime: casesForVentilatorsByRequestedTime(
-        infectionsByRequestedTime(data, currentlyInfected(reportedCases, 10))
-      ),
-      dollarsInFlight: dollarsInFlight(
-        data,
-        infectionsByRequestedTime(data, currentlyInfected(reportedCases, 10))
-      )
-    },
-    severeImpact: {
-      currentlyInfected: currentlyInfected(reportedCases, 50),
-      infectionsByRequestedTime: infectionsByRequestedTime(
-        data,
-        currentlyInfected(reportedCases, 50)
-      ),
-      severeCasesByRequestedTime: severeCasesByRequestedTime(
-        infectionsByRequestedTime(data, currentlyInfected(reportedCases, 50))
-      ),
-      hospitalBedsByRequestedTime: hospitalBedsByRequestedTime(
-        data,
-        severeCasesByRequestedTime(
-          infectionsByRequestedTime(data, currentlyInfected(reportedCases, 50))
-        )
-      ),
-      casesForICUByRequestedTime: casesForICUByRequestedTime(
-        infectionsByRequestedTime(data, currentlyInfected(reportedCases, 50))
-      ),
-      casesForVentilatorsByRequestedTime: casesForVentilatorsByRequestedTime(
-        infectionsByRequestedTime(data, currentlyInfected(reportedCases, 50))
-      ),
-      dollarsInFlight: dollarsInFlight(
-        data,
-        infectionsByRequestedTime(data, currentlyInfected(reportedCases, 50))
-      )
-    }
+    impact,
+    severeImpact
   };
+
+  return result;
 };
+
 export default covid19ImpactEstimator;
