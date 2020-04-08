5b0c885617311a7097dd8578d409285aad1f2f65
Kayode Gabriel Abiodun <kayodegabriela@gmail.com>
2020-04-08 11:55:20 +0100

    Starting Project

--- src/estimator.js
+++ src/estimator.js
@@ -1,26 +1,27 @@
-const covid19ImpactEstimator = (data) => {
-  const input = data;
-  input: {
-    region: {
-      name: 'Africa';
-      avgAge: 19.7;
-      avgDailyIncomeInUSD: 5;
-      avgDailyIncomePopulation: 0.71;
-    }
-    periodType: 'days';
-    timeToElapse: 58;
-    reportedCases: 674;
-    population: 66622705;
-    totalHospitalBeds: 1380614;
-  }
+const input = {
+  region: {
+    name: 'Africa',
+    avgAge: 19.7,
+    avgDailyIncomeInUSD: 5,
+    avgDailyIncomePopulation: 0.71
+  },
+  periodType: 'days',
+  timeToElapse: 58,
+  reportedCases: 674,
+  population: 66622705,
+  totalHospitalBeds: 1380614
+};
+
+const covid19ImpactEstimator = (data = input) => {
+  const { reportedCases } = data;
   return {
-    data: input, //input data I get
+    data,
     impact: {
       currentlyInfected: reportedCases * 10
-    }, //best case estimation
+    },
     severeImpact: {
       currentlyInfected: reportedCases * 50
-    } //sever case estimation
+    }
   };
 };
 
