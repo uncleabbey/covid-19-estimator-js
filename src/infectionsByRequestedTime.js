c33c4454405f28076518b174d8b08660da66588b
Kayode Gabriel Abiodun <kayodegabriela@gmail.com>
2020-04-08 16:09:23 +0100

    Third Attempt

--- src/infectionsByRequestedTime.js
+++ src/infectionsByRequestedTime.js
@@ -3,7 +3,7 @@ const infectionsByRequestedTime = (data, theCurrentlyInfected) => {
   const { periodType, timeToElapse } = data;
   switch (periodType) {
     case 'weeks':
-      infected = theCurrentlyInfected * 2 ** Math.round((timeToElapse * 7) / 3);
+      infected = theCurrentlyInfected * 2 ** Math.floor((timeToElapse * 7) / 3);
       break;
     case 'months':
       infected = theCurrentlyInfected * 2 ** Math.round((timeToElapse * 30) / 3);
